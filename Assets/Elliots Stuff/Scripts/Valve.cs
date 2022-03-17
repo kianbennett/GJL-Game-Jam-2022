b4d68f9cfc0e4e47d93c451ce03657acbd6586dc
elliotjermy <elliot.jermy@outlook.com>
2022-03-17 22:45:44 +0000

    added stuff

---
+++ Assets/Elliots Stuff/Scripts/Valve.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valve : MonoBehaviour
+{
+    private bool valveOn;
+    private Animator anim;
+    private bool steamInRange;
+    private bool springInRange;
+    private bool shrinkInRange;
+    private int activePlayer;
+    public GameObject steam;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        valveOn = false;
+        this.GetComponent<TriggerObject>().active = false;
+    }
+    public void TurnValve()
+    {
+        if (valveOn)
+        {
+            ////Turn off valve
+            //valveOn = false;
+            //anim.SetTrigger("valveOff");
+            //this.GetComponent<TriggerObject>().active = false;
+            //steam.SetActive(true);
+        }
+        else
+        {
+            //Turn on lever
+            valveOn = true;
+            anim.SetTrigger("valveOn");
+            this.GetComponent<TriggerObject>().active = true;
+            steam.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        activePlayer = GameObject.Find("PlayerController").GetComponent<PlayerController>().ActiveCharacter;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (activePlayer == 0 && springInRange)
+            {
+                TurnValve();
+            }
+            if (activePlayer == 1 && steamInRange)
+            {
+                TurnValve();
+            }
+            if (activePlayer == 2 && shrinkInRange)
+            {
+                TurnValve();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (activePlayer == 0)
+            {
+                springInRange = true;
+            }
+            if (activePlayer == 1)
+            {
+                steamInRange = true;
+            }
+            if (activePlayer == 2)
+            {
+                shrinkInRange = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (activePlayer == 0)
+            {
+                springInRange = false;
+            }
+            if (activePlayer == 1)
+            {
+                steamInRange = false;
+            }
+            if (activePlayer == 2)
+            {
+                shrinkInRange = false;
+            }
+        }
+    }
+}
