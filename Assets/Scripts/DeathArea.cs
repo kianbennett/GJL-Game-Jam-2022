ed1b73fbe8e29fb0b3821b2757a0079fcdee62b3
Harley Driver <harleydriver@hotmail.co.uk>
2022-03-17 20:46:35 +0000

    New level
    Adding another game level,

--- Assets/Scripts/DeathArea.cs
+++ Assets/Scripts/DeathArea.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 
 public class DeathArea : MonoBehaviour
 {
+    [Header("Spawn Point Game Object")]
     public GameObject spawnPoint;
-    //public GameObject DeathBox;
-    public GameObject player;
 
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-
-
-
-    }
-
-    public void OnTriggerEnter(Collider other)
+    //info
+    //apply this script to a game object with a collider, make sure you set it's collider to trigger = true
+    //this script takes in a game object which acts as a spawn point to send the player too if they collide with the game object this script is attached too
+    public void OnTriggerEnter(Collider other) // looks for trigger
     {
 
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player") // checks if it has collided with anything Tagged "Player"
         {
-            other.transform.position = spawnPoint.transform.position;
+            other.transform.position = spawnPoint.transform.position; // sends collided player to spawn point location
         }
 
     }
